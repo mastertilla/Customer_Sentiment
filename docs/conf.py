69084c484375137cbba6a70540975c92c43ce9fa
Mastertilla <danielmontillanavas@gmail.com>
2019-04-15 20:42:36 +0100

    Added version number

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Daniel Montilla Navas'
 author = 'Daniel Montilla Navas'
 
 # The short X.Y version
-version = ''
+version = '1.0'
 # The full version, including alpha/beta/rc tags
 release = '2019'
 
