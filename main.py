1e037dd35cd085373cf267e9f809125fb1c24cc3
Daniel.Navas <daniel.navas@tandem.co.uk>
2019-04-16 15:07:34 +0100

    Move functions from dataprep.py to class + modified main.py to include new functions

--- main.py
+++ main.py
@@ -14,22 +14,24 @@ Options:
 
 from src.scrapper import ReviewScrapper
 from src.data_cleaning import DataPrep
+import time
 
 from docopt import docopt
 
 if __name__=="__main__":
+    start = time.time()
     arguments = docopt(__doc__, version='1.0')
-    # print(arguments)
     if arguments['--scrape'] == 'y':
         scrapper = ReviewScrapper(url=u'https://uk.trustpilot.com/review/www.currys.co.uk?page=')
         scrapper.do_all()
         data_prep = DataPrep(data=scrapper.reviews)
     else:
         data_prep = DataPrep()
-    
-    # import pdb; pdb.set_trace()
+
     data_prep.read_data()
     data_prep.parse_document()
-    data_prep.clean_reviews()
-    
-    # print(data_prep.dict_sentences)
+    data_prep.cleaning_reviews()
+    data_prep.return_dataframe()
+
+    print("Took %.2f seconds" %(time.time() - start))
+
